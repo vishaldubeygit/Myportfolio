46c2f4bf2f89e25d90f35717ed20857a47d74bd2
vishaldubeygit <vishal20031228@gmail.com>
2025-07-03 15:30:47 +0530

    Update AboutSection.jsx

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -38,7 +38,7 @@ export const AboutSection = () => {
               </a>
 
               <a
-                href=""
+                href="https://drive.google.com/drive/home?dmr=1&ec=wgc-drive-globalnav-goto"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
                 Download CV
