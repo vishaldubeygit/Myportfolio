021019f028bed2f8f5ac77bcb0c2ed723ff301b9
vishaldubeygit <vishal20031228@gmail.com>
2025-07-03 16:01:07 +0530

    Update AboutSection.jsx

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -38,7 +38,7 @@ export const AboutSection = () => {
               </a>
 
               <a
-                href="https://drive.google.com/file/d/1A55JxSfOtzYrkovNEF6WkNDuaPp-fjAj/view?usp=drive_link"
+                href="https://drive.google.com/file/d/1A55JxSfOtzYrkovNEF6WkNDuaPp-fjAj/view?usp=sharing"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
                 Download CV
